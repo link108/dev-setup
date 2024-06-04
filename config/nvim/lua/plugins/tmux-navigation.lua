8ff9a4f5cb69741e1200289cdda438b3c3c50dc0
Cameron Motevasselani <cameron@armory.io>
2024-06-03 23:02:54 -0700

    updating nvim config

--- config/nvim/lua/plugins/tmux-navigation.lua
+++ config/nvim/lua/plugins/tmux-navigation.lua
@@ -2,9 +2,9 @@ return {
 	"alexghergh/nvim-tmux-navigation",
 	config = function()
 		require("nvim-tmux-navigation").setup({})
-		vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", {})
-		vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", {})
-		vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
-		vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
+		vim.keymap.set("n", "<M-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", {})
+		vim.keymap.set("n", "<M-j>", "<Cmd>NvimTmuxNavigateDown<CR>", {})
+		vim.keymap.set("n", "<M-k>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
+		vim.keymap.set("n", "<M-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
 	end,
 }
--- config/nvim/lua/vim-options.lua
+++ config/nvim/lua/vim-options.lua
@@ -8,7 +8,13 @@ vim.cmd("map s <Nop>")
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
 vim.g.mapleader = " "
 
+-- vim.cmd("let g:tmux_navigator_no_mappings = 1")
+-- vim.cmd("nnoremap <silent> <M-h> :TmuxNavigateLeft<cr>")
+-- vim.cmd("nnoremap <silent> <M-j> :TmuxNavigateDown<cr>")
+-- vim.cmd("nnoremap <silent> <M-k> :TmuxNavigateUp<cr>")
+-- vim.cmd("nnoremap <silent> <M-l> :TmuxNavigateRight<cr>")
 
+-- was not able to get meta key working for nvim 
 -- vim.keymap.set("n", "<M-h>", "<Nop>", { silent = true, remap = true })
 
 -- vim.cmd("nnoremap <M-p> :echom 'p'<cr>")
@@ -17,15 +23,10 @@ vim.g.mapleader = " "
 -- vim.cmd("nnoremap <M-k> :wincmd k<cr>")
 -- vim.cmd("nnoremap <M-l> :wincmd l<cr>")
 
-
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
--- vim.keymap.set('n', '<M-h>', ':wincmd h<CR>')
--- vim.keymap.set('n', '<M-j>', ':wincmd j<CR>')
--- vim.keymap.set('n', '<M-k>', ':wincmd k<CR>')
--- vim.keymap.set('n', '<M-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<M-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<M-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<M-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<M-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
