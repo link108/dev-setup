2567f9f5cd022d2116a64932ea113946a3be892e
Cameron <cmotevasselani@gmail.com>
2025-01-17 14:08:56 -0800

    more window management commandsg

--- config/nvim/lua/vim-options.lua
+++ config/nvim/lua/vim-options.lua
@@ -15,21 +15,20 @@ vim.g.mapleader = " "
 -- vim.cmd("nnoremap <silent> <M-k> :TmuxNavigateUp<cr>")
 -- vim.cmd("nnoremap <silent> <M-l> :TmuxNavigateRight<cr>")
 
--- was not able to get meta key working for nvim 
--- vim.keymap.set("n", "<M-h>", "<Nop>", { silent = true, remap = true })
-
--- vim.cmd("nnoremap <M-p> :echom 'p'<cr>")
--- vim.cmd("nnoremap <M-h> :wincmd h<cr>")
--- vim.cmd("nnoremap <M-j> :wincmd j<cr>")
--- vim.cmd("nnoremap <M-k> :wincmd k<cr>")
--- vim.cmd("nnoremap <M-l> :wincmd l<cr>")
-
 vim.keymap.set('n', '<M-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<M-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<M-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<M-l>', ':wincmd l<CR>')
 
+vim.keymap.set('n', '<M-H>', ':wincmd <<CR>')
+vim.keymap.set('n', '<M-J>', ':wincmd -<CR>')
+vim.keymap.set('n', '<M-K>', ':wincmd +<CR>')
+vim.keymap.set('n', '<M-L>', ':wincmd ><CR>')
+
+vim.keymap.set('n', '<M-=>', ':wincmd =<CR>')
+
+
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
-vim.keymap.set('n', '<M-->', ':split<CR>')
-vim.keymap.set('n', '<M-\\>', ':vsplit<CR>')
+vim.keymap.set('n', '<M-->', ':split<CR><C-w>w')
+vim.keymap.set('n', '<M-\\>', ':vsplit<CR><C-w>w')
